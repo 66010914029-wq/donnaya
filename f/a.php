1c9e71f304ed9841e1b1a6f2cc0525d8f537ec61
donnaya <66010914029@msu.ac.th>
2026-01-28 15:56:55 +0700

    first commit

---
+++ f/a.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>66010914029 ดนยา อุดมคำ(ปูเป้)</title>
+</head>
+
+<h1>66010914029 ดนยา อุดมคำ(ปูเป้)</h1>
+
+<script language="javascript">
+	document.write("คณะการบัญชีและการจัดการ");
+</script>
+<br>
+
+<?php
+	echo "Web Programming <br>";
+	echo "Mahasarakham University <br>";
+	print "Jutaporn Somjan <br>";
+	
+	$name= "จุฑาภรณ์ สมจันทร์";
+	$age= 21;
+	$Name= "จุฑาภรณ์";
+	
+	//echo gettype($age);
+	var_dump($age);
+	echo "<hr>";
+	
+	echo $name. "<br>";
+	echo $Name. "<br>";
+	
+	$a = 10;
+	$b = 5;
+	$c = 3;
+	$x = $a + $b ;
+	echo $x; 
+
+?>
+
+<body>
+</body>
+</html>
+ 
+ 
+ 
+ 
