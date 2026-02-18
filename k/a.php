72d77d5b272fba3c7c667ead4f0add946b231c32
donnaya <66010914029@msu.ac.th>
2026-02-18 16:52:55 +0700

    ฟ

--- k/a.php
+++ k/a.php
@@ -17,7 +17,7 @@
 
 <a href="images/1.jpg" class="btn-green">เปิดรูปตัวเอง</a>
 
-<a href="images/teacher.jpg" class="btn-orange">เปิดรูปอาจารย์ผู้สอน</a>
+<a href="images/2.jpg" class="btn-orange">เปิดรูปอาจารย์ผู้สอน</a>
 
 <h1>งาน k 66010914029 ดนยา อุดมคำ</h1>
 <img src= "images/1.jpg"width="305" >
