a6a985f9007513b300b456fdaf15ac2e89228c77
donnaya <66010914029@msu.ac.th>
2026-02-18 17:02:34 +0700

    ฟ

--- k/a.php
+++ k/a.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <title>งาน k 66010914029 ดนยา อุดมค่า</title>
     <style>
-        /* This adds the required colors for your buttons */
         .btn-green { background-color: #28a745; color: white; padding: 10px; text-decoration: none; border-radius: 5px; display: inline-block; }
         .btn-orange { background-color: #ff8c00; color: white; padding: 10px; text-decoration: none; border-radius: 5px; display: inline-block; }
     </style>
@@ -12,14 +11,14 @@
 
 <body>
     <h1>งาน k 66010914029 ดนยา อุดมค่า</h1>
-    
-    
+   
+    <img src="images/1.jpg" width="305">
 
     <br><br>
 
     <a href="images/1.jpg" class="btn-green">เปิดรูปตัวเอง</a>
-
     <a href="images/2.jpg" class="btn-orange">เปิดรูปอาจารย์ผู้สอน</a>
 </body>
 </html>
+
 
