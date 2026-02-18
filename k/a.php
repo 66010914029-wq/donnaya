40334556887c62311e2290d343808ad514736db9
donnaya <66010914029@msu.ac.th>
2026-02-18 16:50:17 +0700

    ฟ

---
+++ k/a.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>งาน k 66010914029 ดนยา อุดมคำ </title>
+<style>
+        /* This adds the required colors for your buttons */
+        .btn-green { background-color: #28a745; color: white; padding: 10px; text-decoration: none; border-radius: 5px; display: inline-block; }
+        .btn-orange { background-color: #ff8c00; color: white; padding: 10px; text-decoration: none; border-radius: 5px; display: inline-block; }
+    </style>
+</head>
+
+<body>
+<h1>งาน k 66010914029 ดนยา อุดมคำ</h1>
+<img src= "images/1.jpg"width="305" >
+<br><br>
+
+<a href="images/1.jpg" class="btn-green">เปิดรูปตัวเอง</a>
+
+<a href="images/teacher.jpg" class="btn-orange">เปิดรูปอาจารย์ผู้สอน</a>
+
+<h1>งาน k 66010914029 ดนยา อุดมคำ</h1>
+<img src= "images/1.jpg"width="305" >
+</body>
+</html>
